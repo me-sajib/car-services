4eee86cbe550eb2097088503ceafc1e1f153c865
sajib <me.mrsajib@gmail.com>
2022-04-11 21:23:43 +0600

    create multiple components

---
+++ src/Pages/Shared/Header/Nav.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-light"
+      style={{ backgroundColor: "#e3f2fd" }}
+    >
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          Logo
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Features
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Pricing
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
