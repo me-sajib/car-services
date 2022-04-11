11f301495a5b86240ab8c5b4be547b7fc929b343
sajib <me.mrsajib@gmail.com>
2022-04-11 21:55:12 +0600

    services items show in service components

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const Service = () => {
+const Service = ({ items }) => {
+  const { name, description, img } = items;
   return (
-    <div>
-      <h2>service</h2>
+    <div className="col">
+      <div className="card">
+        <img src={img} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">{description}</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Service from "../Service/Service";
 
 const Services = () => {
+  const [service, setService] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setService(data));
+  }, []);
   return (
-    <div>
-      <h1>services items</h1>
+    <div className="container">
+      <h1 className="text-center py-5" style={{ color: "blue" }}>
+        Our Services
+      </h1>
+
+      <div className="row row-cols-1 row-cols-md-2 g-4">
+        {service.map((item) => (
+          <Service key={item.id} items={item} />
+        ))}
+      </div>
     </div>
   );
 };
