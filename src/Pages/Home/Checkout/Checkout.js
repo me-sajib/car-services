b9f6bcd16986f63e72dd3a1938029bedfc4b9cb9
sajib <me.mrsajib@gmail.com>
2022-04-16 11:57:51 +0600

    add checkout page

---
+++ src/Pages/Home/Checkout/Checkout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Checkout = () => {
+    return (
+        <div className='container'>
+            <h1>checkout your product</h1>
+        </div>
+    );
+};
+
+export default Checkout;
