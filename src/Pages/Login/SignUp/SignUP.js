ddb958ce584899590fca9fc9bb068ed3cac76195
sajib <me.mrsajib@gmail.com>
2022-04-16 12:00:09 +0600

    registration authentication successfully

--- src/Pages/Login/SignUp/SignUP.js
+++ src/Pages/Login/SignUp/SignUP.js
@@ -1,27 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import auth from '../../../firebase.init';
 
 const SignUP = () => {
+      const [
+  createUserWithEmailAndPassword,
+  user,
+  loading,
+  error,
+] = useCreateUserWithEmailAndPassword(auth);
+    const createAccount = (e)=>{
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        
+        createUserWithEmailAndPassword(email, password,{sendEmailVerification: true})
+    }
+     if(user){
+        console.log(user)
+    }
+    if(error){
+        console.log(error)
+    }
+    
     return (
         <div className='bg-light py-5'>
             <div className="container">
                 <div className="w-50 mx-auto">
                      <h1>Hi, <span className='text-primary'> Create your account    </span></h1>
-            <form>
+                    <form onSubmit={createAccount}>
                 <div className="mb-3">
-                    <label for="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" />
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="text" name='name' className="form-control" />
                     
                 </div>
                 <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" name='email' className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" name='password' className="form-control" id="exampleInputPassword1"/>
                 </div>
                 
                 <button type="submit" className="btn btn-primary d-block w-100">Submit</button>
