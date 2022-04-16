57cda1c0e44fdb325d270e0579ef4c6249a909c8
sajib <me.mrsajib@gmail.com>
2022-04-16 12:00:42 +0600

    add sign in authentication

--- src/Pages/Login/SignIn/SignIn.js
+++ src/Pages/Login/SignIn/SignIn.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import auth from '../../../firebase.init';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 const SignIn = () => {
+  const [
+  signInWithEmailAndPassword,
+  user,
+  loading,
+  error,
+] = useSignInWithEmailAndPassword(auth);
+    const userLogin = e =>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        signInWithEmailAndPassword(email, password)
+    }
+    if(user){
+        console.log(user, error)
+    }
+   
     return (
         <div className='bg-light py-5'>
             <div className="container">
                 <div className="w-50 mx-auto">
-                     <h1>Hi, <span className='text-primary'> Welcome Back</span></h1>
-            <form>
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                 <h1>Hi, <span className='text-primary'> Welcome Back</span></h1>
+              <form onSubmit={userLogin}>
+                   <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" name='email' className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" name='password' className="form-control" id="exampleInputPassword1"/>
                 </div>
                 <button type="submit" className="btn btn-primary d-block w-100">Login</button>
                 <p className='pt-4'>If your no account? <Link to="/register" className="text-primary">Create account now</Link></p>
