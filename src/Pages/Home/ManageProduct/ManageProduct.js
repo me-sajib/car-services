3303b566722200f78baa1cce7580461ffb3447e5
sajib <me.mrsajib@gmail.com>
2022-04-22 21:53:41 +0600

    delete any service if you want

--- src/Pages/Home/ManageProduct/ManageProduct.js
+++ src/Pages/Home/ManageProduct/ManageProduct.js
@@ -2,15 +2,26 @@ import React from "react";
 import useServices from "../../../Hooks/UseServices";
 
 const ManageProduct = () => {
-  const [service] = useServices();
+  const [service, setService] = useServices();
   const deleteService = (id) => {
-    console.log(id);
+    const sure = window.confirm("are you sure?");
+    // if user is ok to data delete then data delete
+    if (sure) {
+      fetch(`http://localhost:5000/service/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const remaining = service.filter((item) => item._id !== id);
+          setService(remaining);
+        });
+    }
   };
   return (
-    <div>
+    <div className="text-center">
       <h1>Any action to products</h1>
       {service.map((sr) => (
-        <div>
+        <div key={sr._id}>
           <h3 className="text-primary">
             {sr.name}{" "}
             <button
