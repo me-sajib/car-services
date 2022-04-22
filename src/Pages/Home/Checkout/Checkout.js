d97956e0ed89f50ddb1a8f0f3011d03a82bfe24d
sajib <me.mrsajib@gmail.com>
2022-04-22 21:18:33 +0600

    fetch data using by id

--- src/Pages/Home/Checkout/Checkout.js
+++ src/Pages/Home/Checkout/Checkout.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import PageTitle from "../../Shared/PageTitle/PageTitle";
 
 const Checkout = () => {
+  const id = useParams().id;
+  const [myService, setMyService] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/service/${id}`)
+      .then((res) => res.json())
+      .then((data) => setMyService(data[0]));
+  }, []);
   return (
     <div className="container">
       <PageTitle title="checkout -car service" />
-      <h1>checkout your product</h1>
+      <div className="py-2">
+        <h1>You want to {myService.name}</h1>
+        <p>{myService.description}</p>
+        <img src={myService.img} alt="" />
+      </div>
+      <button className="btn btn-info">CHECKOUT</button>
     </div>
   );
 };
