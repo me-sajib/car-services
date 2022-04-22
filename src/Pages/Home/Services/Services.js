39ee77d0e09bb2145fd1058c6fe24c266d22afd5
sajib <me.mrsajib@gmail.com>
2022-04-22 20:41:52 +0600

    fetch data from mongodb

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -5,7 +5,7 @@ const Services = () => {
   const [service, setService] = useState([]);
 
   useEffect(() => {
-    fetch("services.json")
+    fetch("http://localhost:5000/service")
       .then((res) => res.json())
       .then((data) => setService(data));
   }, []);
